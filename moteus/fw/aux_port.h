6e2b325063600326c64efd94a32e8d1429a61d3f
D3721s <88261082+d3721s@users.noreply.github.com>
2026-06-03 19:32:22 +0800

    Update aux_port.h

--- moteus/fw/aux_port.h
+++ moteus/fw/aux_port.h
@@ -1322,7 +1322,6 @@ class AuxPort {
               (config_.spi.trim == T::kTrimX) ? KTH7812::kTrimX :
               (config_.spi.trim == T::kTrimY) ? KTH7812::kTrimY :
               KTH7812::kTrimNone;
-          options.trim = KTH7812::kTrimY;
           if (config_.spi.mode == aux::Spi::Config::kOnboardKth7812) {
             options.mgh = g_hw_pins.kth7812_mgh;
             options.mgl = g_hw_pins.kth7812_mgl;
